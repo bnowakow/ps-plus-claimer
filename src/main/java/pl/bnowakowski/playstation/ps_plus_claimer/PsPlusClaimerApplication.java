4e4227afd6f4d5a7cc70ff82bb0b6cf4fbfcd597
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2021-02-24 23:19:12 +0100

    start.spring.io

---
+++ src/main/java/pl/bnowakowski/playstation/ps_plus_claimer/PsPlusClaimerApplication.java
@@ -0,0 +1,13 @@
+package pl.bnowakowski.playstation.ps_plus_claimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsPlusClaimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsPlusClaimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bnowakowski/playstation/ps_plus_claimer/PsPlusClaimerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bnowakowski.playstation.ps_plus_claimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsPlusClaimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
