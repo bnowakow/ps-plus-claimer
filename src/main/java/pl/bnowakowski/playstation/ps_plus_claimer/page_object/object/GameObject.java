b7eafde07874b7ef3011a9032036de35e9577e12
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2021-11-10 17:33:53 +0100

    wip

--- src/main/java/pl/bnowakowski/playstation/ps_plus_claimer/page_object/object/GameObject.java
+++ src/main/java/pl/bnowakowski/playstation/ps_plus_claimer/page_object/object/GameObject.java
@@ -27,15 +27,18 @@ public class GameObject extends PageObjectObject {
 //        gamePage = new GamePage(driver, "https://www.playstation.com/pl-pl/playstation-network/");
 //        gamePage = new GamePage(driver, "https://pl-gmsg.np.community.playstation.net/groupMessaging/v1/users/supach_/systemMessages/messages?fields=%40default%2CmessageUid%2CseenFlag%2CopenFlag%2ClastCheckDate%2CurlLink%2CextendBody%2CcontentKeys&toPlatform=PS4&cacheBust=1616182098360&npLanguage=pl");
 //        gamePage = new GamePage(driver, "https://web.np.playstation.com/api/graphql/v1/op?operationName=oracleUserProfileRetrieve&variables=%7B%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22ecebc783d1128d899231b2610d31ade32f43ddc7a86de36b219d516c9e0c21fb%22%7D%7D");
+        gamePage = new GamePage(driver, "https://store.playstation.com/");
+//        gamePage = new GamePage(driver, "https://my.account.sony.com/central/signin/?client_id=e4a62faf-4b87-4fea-8565-caaabb3ac918&scope=web%3Acore&access_type=offline&state=3f07ef0430a7a1b40ac38e0fba4e9cedd8128c66f2471f86f06dd77b716e987b&service_entity=urn%3Aservice-entity%3Apsn&ui=pr&smcid=web%3Astore&redirect_uri=https%3A%2F%2Fweb.np.playstation.com%2Fapi%2Fsession%2Fv1%2Fsession%3Fredirect_uri%3Dhttps%253A%252F%252Fstore.playstation.com%252Fpl-pl%252Flatest%26x-psn-app-ver%3D%2540sie-ppr-web-session%252Fsession%252Fv5.5.0&auth_ver=v3&error=login_required&error_code=4165&no_captcha=true/signin/ca?entry=ca#/signin/ca?entry=ca");
     }
 
     @RequestMapping(value = "/login")
     public String login() {
-        gamePage = new GamePage(driver, "https://web.np.playstation.com/api/graphql/v1/op?operationName=oracleUserProfileRetrieve&variables=%7B%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22ecebc783d1128d899231b2610d31ade32f43ddc7a86de36b219d516c9e0c21fb%22%7D%7D");
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+//        gamePage = new GamePage(driver, "https://store.playstation.com/");
+//        WebDriverWait wait = new WebDriverWait(driver, 10);
 
         this.setupCookie();
-        this.driver.get("https://web.np.playstation.com/api/graphql/v1/op?operationName=oracleUserProfileRetrieve&variables=%7B%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22ecebc783d1128d899231b2610d31ade32f43ddc7a86de36b219d516c9e0c21fb%22%7D%7D");
+        this.driver.get("https://store.playstation.com/pl-pl/product/EP2333-PPSA01832_00-MAQUETTESIEE0000");
+//        this.driver.get("https://my.account.sony.com/central/signin/?client_id=e4a62faf-4b87-4fea-8565-caaabb3ac918&scope=web%3Acore&access_type=offline&state=3f07ef0430a7a1b40ac38e0fba4e9cedd8128c66f2471f86f06dd77b716e987b&service_entity=urn%3Aservice-entity%3Apsn&ui=pr&smcid=web%3Astore&redirect_uri=https%3A%2F%2Fweb.np.playstation.com%2Fapi%2Fsession%2Fv1%2Fsession%3Fredirect_uri%3Dhttps%253A%252F%252Fstore.playstation.com%252Fpl-pl%252Flatest%26x-psn-app-ver%3D%2540sie-ppr-web-session%252Fsession%252Fv5.5.0&auth_ver=v3&error=login_required&error_code=4165&no_captcha=true/signin/ca?entry=ca#/signin/ca?entry=ca");
         return "foobar";
     }
 
--- src/main/java/pl/bnowakowski/playstation/ps_plus_claimer/page_object/object/PageObjectObject.java
+++ src/main/java/pl/bnowakowski/playstation/ps_plus_claimer/page_object/object/PageObjectObject.java
@@ -50,8 +50,58 @@ public class PageObjectObject {
     }
 
     public void setupCookie() {
-        Cookie cookie = new Cookie("pdccws_p", userToken);
+        Cookie cookie;
+
+        cookie = new Cookie("isSigned", "true");
         driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("mbox", "PC#39fdc612f9a046b49b2c0bb95498d0ce.35_0#1678783792|session#58a75da4fd584433840eea6b9905dbd5#1616183432");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("pdc_custage", "m");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("pdccws_p", "s%3AaKbG8t1aMndeWJajNErSrWSF_cZQq6Mo.UsPWUw8M7lxRMWuHuj%2Bk37K5gGij0%2B5IgCQSLhjNUL8");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("s_cc", "true");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("s_ecid", "MCMID%7C17586022103806991802880536921346660212");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("s_fid", "0F80D731A3A56686-306C8A536B079D1B");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("s_sq", "snepsnsocial-prod%3D%2526c.%2526a.%2526activitymap.%2526page%253Dhttps%25253A%25252F%25252Fmy.playstation.com%25252F%2526link%253DOK%2526region%253Dember590%2526.activitymap%2526.a%2526.c%26snepdcglobal-prod%3D%2526c.%2526a.%2526activitymap.%2526page%253Dweb%25253Apdc%25253Aplaystation-network%2526link%253DBartek%252520Nowakowski%252520supach_%2526region%253Dsb-social-toolbar-root%2526pageIDType%253D1%2526.activitymap%2526.a%2526.c%2526pid%253Dweb%25253Apdc%25253Aplaystation-network%2526pidt%253D1%2526oid%253Dhttps%25253A%25252F%25252Fmy.playstation.com%25252F%2526ot%253DA");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("uh", "4c7824d882b7cb363b04f01b30af3697df7cc28ad5cafcec5b037a7057be5052\n");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("at_check", "true");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("euconsent-v2", "CO9K0l8PDUbyeASABCENBSCsAP_AAG_AAB5YHnQBQAiAEFAI6AjUBeYHnQAwAiAEFAXmAAA.YAAAAAAAAAAA");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("eucookiepreference", "accept");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("_evidon_consent_cookie", "{\"consent_date\":\"2021-03-19T18:52:00.636Z\",\"categories\":{\"18\":true},\"vendors\":{\"18\":{}},\"cookies\":{\"18\":true},\"consent_type\":1}");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("AMCV_BD260C0F53C9733E0A490D45%40AdobeOrg", "-1124106680%7CMCIDTS%7C18706%7CMCMID%7C17586022103806991802880536921346660212%7CMCAAMLH-1616784714%7C6%7CMCAAMB-1616784714%7C6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y%7CMCOPTOUT-1616187114s%7CNONE%7CMCAID%7CNONE%7CMCSYNCSOP%7C411-18713%7CvVersion%7C5.2.0");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("AMCVS_BD260C0F53C9733E0A490D45%40AdobeOrg", "1");
+        driver.manage().addCookie(cookie);
+
+        cookie = new Cookie("_fbp", "fb.1.1615538978843.30361724");
+        driver.manage().addCookie(cookie);
+
+
+
     }
 
     // https://webelement.click/en/http_header_selenium_webdriver_basic_authorization
